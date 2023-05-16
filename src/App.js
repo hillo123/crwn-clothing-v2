fdcff2f05c65bf6db702d08555d50012bf86c2e3
Guillermo <guillermozayas2002@gmail.com>
2023-05-16 23:22:43 +0200

    This is a new commit for what I originally planned to be an amendmend- add shop component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
+import Shop from './components/shop/shop.component';
 
-const Shop =()=>{
-  return <h1>SHOPS</h1>
-}
+
 const App = () => {
   return (
     <Routes>

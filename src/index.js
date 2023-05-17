5bbe4cbc6e8971ef248b29e1e2e52273c47898f1
Guillermo <guillermozayas2002@gmail.com>
2023-05-17 23:57:48 +0200

    creating a product context

--- src/index.js
+++ src/index.js
@@ -6,16 +6,17 @@ import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from './context/user.context';
+import { ProductsContext, ProductsProvider } from './context/products.context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <UserProvider>
-   
-    <App />
-      
-    </UserProvider>
+      <UserProvider>
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
